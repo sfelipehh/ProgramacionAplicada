29d52b7fcec424be3b4514ddb197e7d3c1fb9241
sfelipehh <sfelipehh@gmail.com>
2022-10-07 21:12:30 -0500

    Ejemplo figura geometrica

---
+++ src/main/java/com/sa/programacionaplicada/logica/Esfera.java
@@ -0,0 +1,20 @@
+package com.sa.programacionaplicada.logica;/*Author:sfeli*/
+
+public class Esfera {
+    private double radio;
+    private double volumen;
+
+    public double calcularVolumen(){
+        //calculo
+        return 0;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double getVolumen() {
+        return volumen;
+    }
+
+}
