ad2ef35724e05ffeab8da92266904f029bc38f62
Ana <analondonom.117@gmail.com>
2022-10-07 19:57:14 -0500

    Funciones calculadora Calculadora resistencias

--- src/main/java/com/sa/programacionaplicada/logica/OperacionesMatematicas.java
+++ src/main/java/com/sa/programacionaplicada/logica/OperacionesMatematicas.java
@@ -2,22 +2,41 @@ package com.sa.programacionaplicada.logica;
 
 public class OperacionesMatematicas {
 
-    public static double  calcularpotencia(double numero, double potencia){
-        double resultado=1;
-        for (int i=1; i<=potencia; i++){
-            resultado= resultado * numero;
+    public static double calcularpotencia(double numero, double potencia) {
+        double resultado = 1;
+        for (int i = 1; i <= potencia; i++) {
+            resultado = resultado * numero;
         }
         return resultado;
     }
 
 
-    public static double calcularraiz (double numero){
+    public static double calcularraiz(double numero) {
 
         double raiz;
-        raiz= Math.sqrt(numero);
+        raiz = Math.sqrt(numero);
         return raiz;
     }
 
+    public void double (double a) {
+        double resultado = Math.log(a);
+        return resultado;
+    }
+
+    public static double logaritmonatural(double a) {
+        double resultado = Math.log(a);
+        return resultado;
+    }
+
+    public static double modulo(double a) {
+        double resultado = Math.abs(a);
+        return resultado;
+    }
+
+    public static double logaritmo(double a) {
+        double resultado = Math.log10(a);
+        return resultado;
+    }
 
 }
 
